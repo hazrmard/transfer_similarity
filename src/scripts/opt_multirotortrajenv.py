c83536e09221598c594946c79bdabc8140dadb4d
Ibrahim <ibrahim78786@gmail.com>
2023-03-13 13:44:37 -0500

    PID and RL parameter optimization scripts update with shared parameters

--- src/scripts/opt_multirotortrajenv.py
+++ src/scripts/opt_multirotortrajenv.py
@@ -2,86 +2,160 @@ import sys
 import os
 import multiprocessing as mp
 from argparse import ArgumentParser, Namespace
+import pickle
 
 import optuna
 import numpy as np
 from rl import learn_rl, evaluate_rl
+from stable_baselines3.common.callbacks import BaseCallback
 from systems.multirotor import Multirotor, MultirotorTrajEnv, VP
-from multirotor.controller import (
-    AltController, AltRateController,
-    PosController, AttController,
-    VelController, RateController,
-    Controller
-)
 
-from .opt_pidcontroller import get_study, get_controller as get_controller_base
+from .opt_pidcontroller import (
+    get_controller as get_controller_base,
+    apply_params as apply_params_pid,
+    make_controller_from_trial,
+    make_env,
+    DEFAULTS as PID_DEFAULTS
+)
 from .setup import local_path
 
 DEFAULTS = Namespace(
     ntrials = 100,
     nprocs = 5,
-    bounding_box = 20.,
-    max_velocity = 5.,
-    max_acceleration = 10.,
-    max_tilt = np.pi / 12,
-    max_angular_acc = 5.,
+    bounding_box = PID_DEFAULTS.bounding_box,
+    max_velocity = PID_DEFAULTS.max_velocity,
+    max_acceleration = PID_DEFAULTS.max_acceleration,
+    max_tilt = PID_DEFAULTS.max_tilt,
+    scurve = PID_DEFAULTS.scurve,
+    use_yaw = PID_DEFAULTS.use_yaw,
+    wind = PID_DEFAULTS.wind,
     num_sims = 5,
+    max_steps = 50_000,
     study_name = 'MultirotorTrajEnv',
+    pid_params = ''
 )
 
+class Callback(BaseCallback):
+
+    def __init__(self, trial: optuna.Trial, verbose: int = 0):
+        super().__init__(verbose)
+        self.trial = trial
+        self.should_stop = False
+        self.rollouts = 0
+
+    def _on_rollout_end(self):
+        self.rollouts += 1
+        self.trial.report(
+            value=np.nanmean([info['r'] for info in self.model.ep_info_buffer]),
+            step=self.rollouts
+        )
+        self.should_stop = self.trial.should_prune()
+
+    def _on_step(self) -> bool:
+        return not self.should_stop
 
 
-def get_controller(m: Multirotor, args: Namespace=DEFAULTS):
-    ctrl = get_controller_base(m, args=args)
+
+def get_study(study_name: str=DEFAULTS.study_name, seed:int=0, args: Namespace=DEFAULTS):
+    storage_name = "sqlite:///studies/{}.db".format(study_name)
+    study = optuna.create_study(
+        study_name=study_name, direction='maximize',
+        storage=storage_name,
+        load_if_exists=True,
+        sampler=optuna.samplers.TPESampler(seed=seed),
+        pruner=optuna.pruners.MedianPruner(
+            n_startup_trials=5,
+            n_warmup_steps=20, # number of rollouts
+            n_min_trials=1
+        )
+    )
+    return study
+
+
+
+def get_controller(m: Multirotor, trial: optuna.Trial, args: Namespace=DEFAULTS):
+    ctrl = get_controller_base(m, scurve=args.scurve, args=args)
     # Taken from PID optimization script, given the default controller params
-    ctrl.ctrl_p.k_p[:] = 18.28
-    ctrl.ctrl_p.k_d[:] = 75
-    ctrl.ctrl_r.k_p[:] = 22.97
-    ctrl.ctrl_r.k_d[:] = 1.81
+    if isinstance(args.pid_params, str) and len(args.pid_params) > 0:
+        with open(args.pid_params, 'rb') as f:
+            params = pickle.load(f)
+    elif isinstance(args.pid_params, dict):
+        params = args.pid_params
+    else:
+        params = make_controller_from_trial(trial=trial, args=args, prefix='pid-')
+    ctrl.set_params(**params)
     return ctrl
 
 
+
 def make_objective(args: Namespace=DEFAULTS):
-    max_steps = 50_000
     def objective(trial: optuna.Trial):
         env_kwargs = dict(
             steps_u = trial.suggest_int('steps_u', 1, 50, step=5),
-            scaling_factor = trial.suggest_float('scaling_factor', 0.05, 1., step=0.01),
+            bounding_box = args.bounding_box,
+            scaling_factor = trial.suggest_float('scaling_factor', 0.05, 1., step=0.05),
             seed=0,
-            get_controller_fn=lambda m: get_controller(m, args),
+            get_controller_fn=lambda m: get_controller(m, trial, args),
             vp = VP
         )
-        env = MultirotorTrajEnv(**env_kwargs)
-        ep_len = env.period//(env.dt * env.steps_u)
-        steps = max_steps
+        env = make_env(env_kwargs, args)
+        ep_len = env.period // (env.dt * env.steps_u)
+        ep_len = ep_len + (32 - (ep_len % 32)) # len as a multiple of 32
         learning_rate = trial.suggest_float('learning_rate', 1e-3, 1e-1, log=True)
-        n_steps = trial.suggest_int('n_steps', ep_len, 5 * ep_len, step=100)
         n_epochs = trial.suggest_int('n_epochs', 1, 5)
+        n_steps = trial.suggest_int('n_steps', max(ep_len, 32), 10*ep_len, step=ep_len)
         batch_size = trial.suggest_int('batch_size', 32, min(256, n_steps), step=32)
         learn_kwargs = dict(
-            steps = steps,
+            steps = args.max_steps,
             n_steps = n_steps,
             learning_rate = learning_rate,
             n_epochs = n_epochs,
             batch_size = batch_size,
             seed=0,
             log_env_params = ('steps_u', 'scaling_factor'),
-            tensorboard_log = env.name + ('/optstudy/%03d' % trial.number),
+            tensorboard_log = env.name + ('/optstudy/%s/%03d' % (args.study_name, trial.number)),
             policy_kwargs=dict(squash_output=False,
-                                net_arch=[dict(pi=[128,128], vf=[128,128])])
+                                net_arch=[dict(pi=[128,128], vf=[128,128])]),
+            callback = Callback(trial=trial)
         )
-        agent = learn_rl(env, **learn_kwargs)
+        agent = learn_rl(env, progress_bar=False, **learn_kwargs)
         agent.save(agent.logger.dir + '/agent')
-        rew, std, time = evaluate_rl(agent, MultirotorTrajEnv(**env_kwargs), args.num_sims)
+        rew, std, time = evaluate_rl(agent, make_env(env_kwargs, args), args.num_sims)
         return rew
     return objective
 
 
 
-def optimize(study: optuna.Study, args: Namespace=DEFAULTS):
-    study.optimize(make_objective(args), n_trials=args.ntrials//args.nprocs)
+def optimize(args: Namespace=DEFAULTS, seed: int=0):
+    study = get_study(args.study_name, seed=seed, args=args)
+    study.optimize(
+        make_objective(args),
+        n_trials=args.ntrials//args.nprocs,
+    )
     return study
 
+
+
+def apply_params(env: MultirotorTrajEnv, **params):
+    prefix = 'pid-'
+    prefix_optional = True
+    env_params = ('steps_u', 'scaling_factor')
+    rl_params = ('learning_rate', 'n_epochs', 'n_steps', 'batch_size')
+    env_dict = {k: params[k] for k in env_params}
+    rl_dict = {k: params[k] for k in rl_params}
+
+    pid_dict = {}
+    for k, v in params.items():
+        if k in env_params or k in rl_params:
+            continue
+        if k.startswith(prefix) and prefix_optional:
+            k = k[len(prefix):]
+        pid_dict[k] = v
+    apply_params_pid(env.ctrl, **pid_dict)
+
+    env.scaling_factor = env_dict['scaling_factor']
+    env.steps_u = env_dict['steps_u']
+    return dict(rl=rl_dict, pid=pid_dict, env=env_dict)
 
 
 if __name__=='__main__':
@@ -91,27 +165,34 @@ if __name__=='__main__':
     parser.add_argument('--ntrials', help='Number of trials.', default=DEFAULTS.ntrials, type=int)
     parser.add_argument('--max_velocity', default=DEFAULTS.max_velocity, type=float)
     parser.add_argument('--max_acceleration', default=DEFAULTS.max_acceleration, type=float)
-    parser.add_argument('--max_angular_acc', default=DEFAULTS.max_angular_acc, type=float)
     parser.add_argument('--max_tilt', default=DEFAULTS.max_tilt, type=float)
+    parser.add_argument('--scurve', action='store_true', default=DEFAULTS.scurve)
+    parser.add_argument('--use_yaw', action='store_true', default=DEFAULTS.use_yaw)
+    parser.add_argument('--wind', help='wind force from heading "force@heading"', default=DEFAULTS.wind)
     parser.add_argument('--bounding_box', default=DEFAULTS.bounding_box, type=float)
     parser.add_argument('--num_sims', default=DEFAULTS.num_sims, type=int)
+    parser.add_argument('--max_steps', default=DEFAULTS.max_steps, type=int)
     parser.add_argument('--append', action='store_true', default=False)
+    parser.add_argument('--pid_params', help='File to load pid params from.', type=str, default=DEFAULTS.pid_params)
+    parser.add_argument('--comment', help='Comments to attach to study.', type=str, default='')
     args = parser.parse_args()
 
     if not args.append:
         import shutil
-        path = local_path / 'tensorboard/MultirotorTrajEnv/optstudy'
+        path = local_path / ('tensorboard/MultirotorTrajEnv/optstudy/' + args.study_name)
         shutil.rmtree(path=path, ignore_errors=True)
         try:
-            os.remove(local_path / (args.study_name + '.db'))
+            os.remove(local_path / ('studies/' + args.study_name + '.db'))
         except OSError:
             pass
-        
-    study = get_study(args.study_name)
+    
+    # create study if it doesn't exist. The study will be reused with a new seed
+    # by each process
+    study = get_study(args.study_name, args=args)
 
     for key in vars(args):
         study.set_user_attr(key, getattr(args, key))
 
     mp.set_start_method('spawn')
     with mp.Pool(args.nprocs) as pool:
-        pool.starmap(optimize, [(study, args) for _ in range(args.nprocs)])
+        pool.starmap(optimize, [(args, i) for i in range(args.nprocs)])
--- src/scripts/opt_pidcontroller.py
+++ src/scripts/opt_pidcontroller.py
@@ -1,60 +1,66 @@
 import os
 import multiprocessing as mp
 from argparse import ArgumentParser, Namespace
-from typing import Callable
+from typing import Callable, Dict, Union
+from pprint import pprint as print
+import pickle
 
 import numpy as np
 import optuna
 from systems.multirotor import Multirotor, MultirotorTrajEnv, VP
 from multirotor.trajectories import Trajectory
 from multirotor.helpers import DataLog
+from multirotor.coords import direction_cosine_matrix, inertial_to_body
 from multirotor.controller import (
     AltController, AltRateController,
     PosController, AttController,
     VelController, RateController,
-    Controller
+    Controller,
+    SCurveController
 )
 
 from .setup import local_path
 
 DEFAULTS = Namespace(
-    ntrials = 100,
+    ntrials = 1000,
     nprocs = 5,
     bounding_box = 20.,
-    max_velocity = 5.,
-    max_acceleration = 10.,
+    max_velocity = 3.,
+    max_acceleration = 2.5,
     max_tilt = np.pi / 12,
-    max_angular_acc = 5.,
-    num_sims = 5,
+    scurve = False,
+    use_yaw = False,
+    wind = '0@0',
+    num_sims = 10,
     study_name = 'MultirotorPIDController',
 )
 
-def get_study(study_name: str):
-    storage_name = "sqlite:///{}.db".format(study_name)
+def get_study(study_name: str=DEFAULTS.study_name, seed: int=0):
+    storage_name = "sqlite:///studies/{}.db".format(study_name)
     study = optuna.create_study(
-        study_name=study_name, direction='minimize',
+        study_name=study_name, direction='maximize',
         storage=storage_name,
-        load_if_exists=True
+        load_if_exists=True,
+        sampler=optuna.samplers.TPESampler(seed=seed)
     )
     return study
 
 
 
 def run_sim(
-    env, traj: 'multirotor.trajectories.Trajectory',
-    ctrl_fn: Callable[[np.ndarray], np.ndarray],
-    max_steps=2_000
+    env, traj: Trajectory,
+    ctrl_fn: Callable[[np.ndarray], np.ndarray]
 ) -> DataLog:
     log = DataLog(env.vehicle, env.ctrl,
-                  other_vars=())
+                  other_vars=('reward',))
         
     for i, (pos, feed_forward_vel) in enumerate(traj):
         # Get prescribed normalized action for system as thrust and torques
         action = ctrl_fn(env.state)
         # Send speeds to environment
         state, r, done, *_ = env.step(action)
-        log.log()
-        if done or i >=max_steps-1:
+        log.log(reward=r)
+        if done:
             break
 
     log.done_logging()
@@ -62,25 +68,24 @@ def run_sim(
 
 
 
-def get_controller(m: Multirotor, args: Namespace=DEFAULTS):
+def get_controller(m: Multirotor, scurve=False, args: Namespace=DEFAULTS):
     assert m.simulation.dt <= 0.1, 'Simulation time step too large.'
     pos = PosController( # PD
-        0.8, 0., 3.75, 100., vehicle=m,
+        0.8, 0., 3.75, args.max_velocity, vehicle=m,
         max_velocity=args.max_velocity,
         max_acceleration=args.max_acceleration,
         square_root_scaling=False
     )
     vel = VelController( # P
-        1, 0., 0, 10.,
+        1, 0., 0, args.max_acceleration,
         vehicle=m,
         max_tilt=args.max_tilt)
     att = AttController( # P
-        1., 0, 0.,
+        [1., 1., 0.], 0, 0., # yaw param is set to 0, in case use_yaw=False
         1., vehicle=m)
     rat = RateController( # PD
-        4, 0, 40,
+        [4, 4, 0], 0, [40, 40, 0], # yaw param is set to 0, in case use_yaw=False
         0.5,
-        max_acceleration=args.max_angular_acc,
         vehicle=m)
 
     alt = AltController(
@@ -93,28 +98,106 @@ def get_controller(m: Multirotor, args: Namespace=DEFAULTS):
 
     ctrl = Controller(
         pos, vel, att, rat, alt, alt_rate,
-        interval_p=0.1, interval_a=0.01, interval_z=0.1
+        period_p=0.1, period_a=0.01, period_z=0.1,
     )
+    if scurve:
+        return SCurveController(ctrl)
     return ctrl
+
+
+
+def make_controller_from_trial(trial: optuna.Trial, args: Namespace=DEFAULTS, prefix=''):
+    r_pitch_roll_p = trial.suggest_float(prefix + 'r_pitch_roll.k_p', 0.1, 50)
+    r_pitch_roll_i = trial.suggest_float(prefix + 'r_pitch_roll.k_i', 0.1, 10)
+    r_pitch_roll_d = trial.suggest_float(prefix + 'r_pitch_roll.k_d', 1, 250)
+    r_pitch_roll_max_acc = trial.suggest_float(prefix + 'r_pitch_roll.max_acceleration', 0.1, 25)
+    if args.use_yaw:
+        r_yaw_p = trial.suggest_float(prefix + 'r_yaw.k_p', 0.1, 5)
+        r_yaw_i = trial.suggest_float(prefix + 'r_yaw.k_i', 0.1, 1)
+        r_yaw_d = trial.suggest_float(prefix + 'r_yaw.k_d', 1, 25)
+        r_yaw_max_acc = trial.suggest_float(prefix + 'r_yaw.max_acceleration', 0.1, 5)
+    else:
+        r_yaw_p, r_yaw_i, r_yaw_d, r_yaw_max_acc = 0, 0, 0, 0
+
+    a_pitch_roll_p = trial.suggest_float(prefix + 'a_pitch_roll.k_p', 0.1, 50)
+    a_pitch_roll_i = trial.suggest_float(prefix + 'a_pitch_roll.k_i', 0.1, 10)
+    a_pitch_roll_d = trial.suggest_float(prefix + 'a_pitch_roll.k_d', 1, 250)
+    if args.use_yaw:
+        a_yaw_p = trial.suggest_float(prefix + 'a_yaw.k_p', 0.1, 5)
+        a_yaw_i = trial.suggest_float(prefix + 'a_yaw.k_i', 0.1, 1)
+        a_yaw_d = trial.suggest_float(prefix + 'a_yaw.k_d', 1, 25)
+    else:
+        a_yaw_p, a_yaw_i, a_yaw_d = 0, 0, 0
+
+    params = dict(
+        ctrl_p = dict(
+            k_p = trial.suggest_float(prefix + 'p.k_p', 0.1, 50),
+            k_i = trial.suggest_float(prefix + 'p.k_i', 0.1, 10),
+            k_d = trial.suggest_float(prefix + 'p.k_d', 1, 250),
+        ),
+        ctrl_v = dict(
+            k_p = trial.suggest_float(prefix + 'v.k_p', 0.1, 50),
+            k_i = trial.suggest_float(prefix + 'v.k_i', 0.1, 10),
+            k_d = trial.suggest_float(prefix + 'v.k_d', 1, 250),
+        ),
+        ctrl_a = dict(
+            k_p = np.asarray((a_pitch_roll_p, a_pitch_roll_p, a_yaw_p)),
+            k_i = np.asarray((a_pitch_roll_i, a_pitch_roll_i, a_yaw_i)),
+            k_d = np.asarray((a_pitch_roll_d, a_pitch_roll_d, a_yaw_d)),
+        ),
+        ctrl_r = dict(
+            k_p = np.asarray((r_pitch_roll_p, r_pitch_roll_p, r_yaw_p)),
+            k_i = np.asarray((r_pitch_roll_i, r_pitch_roll_i, r_yaw_i)),
+            k_d = np.asarray((r_pitch_roll_d, r_pitch_roll_d, r_yaw_d)),
+            max_acceleration = np.asarray((r_pitch_roll_max_acc, r_pitch_roll_max_acc, r_yaw_max_acc)),
+            max_err_i = np.asarray((r_pitch_roll_max_acc, r_pitch_roll_max_acc, r_yaw_max_acc)),
+        ),
+        # ctrl_z = dict(
+        #     k_p = trial.suggest_float('z.k_p', 0.1, 50),
+        #     k_i = trial.suggest_float('z.k_i', 0.1, 10),
+        #     k_d = trial.suggest_float('z.k_d', 1, 250),
+        # ),
+        # ctrl_vz = dict(
+        #     k_p = trial.suggest_float('vz.k_p', 0.1, 50),
+        #     k_i = trial.suggest_float('vz.k_i', 0.1, 10),
+        #     k_d = trial.suggest_float('vz.k_d', 1, 250),
+        # ),
+    )
+    if args.scurve:
+        params.update({prefix + 'feedforward_weight': trial.suggest_float(prefix + 'feedforward_weight', 0.1, 1.0, step=0.1)})
+    return params
+
+
+
+def make_env(env_params, args: Union[Namespace,Dict]=DEFAULTS):
+    if isinstance(args, dict):
+        args = Namespace(**args)
+    if args.wind != DEFAULTS.wind:
+        force, heading = args.wind.split('@')
+        force, heading = float(force), float(heading) * np.pi / 180
+        wforce = force * np.asarray([-np.cos(heading), -np.sin(heading), 0])
+        def wind_fn(m: Multirotor):
+            dcm = direction_cosine_matrix(*m.orientation).astype(m.dtype)
+            return inertial_to_body(wforce.astype(m.dtype), dcm)
+    else:
+        def wind_fn(m: Multirotor):
+            return 0.
+    env_params['disturbance_fn'] = wind_fn
+    env = MultirotorTrajEnv(**env_params)
+    return env
+
 
 
 def make_objective(args: Namespace=DEFAULTS):
     env_kwargs = dict(
         bounding_box=args.bounding_box,
         seed=0,
-        get_controller_fn=lambda m: get_controller(m, args)
+        get_controller_fn=lambda m: get_controller(m, args.scurve, args)
     )
     def objective(trial: optuna.Trial):
-        pos_p = trial.suggest_float('pos_p', 0.1, 50)
-        pos_d = trial.suggest_float('pos_d', 1, 250)
-        rat_p = trial.suggest_float('rat_p', 0.1, 50)
-        rat_d = trial.suggest_float('rat_d', 1, 250)
-        
-        env = MultirotorTrajEnv(**env_kwargs)
-        env.ctrl.ctrl_p.k_p[:] = pos_p
-        env.ctrl.ctrl_p.k_d[:] = pos_d
-        env.ctrl.ctrl_r.k_p[:] = rat_p
-        env.ctrl.ctrl_r.k_d[:] = rat_d
+        ctrl_params = make_controller_from_trial(trial=trial, args=args)
+        env = make_env(env_kwargs, args)
+        env.ctrl.set_params(**ctrl_params)
         errs = []
         for i in range(args.num_sims):
             env.reset()
@@ -122,23 +205,48 @@ def make_objective(args: Namespace=DEFAULTS):
             traj = Trajectory(env.vehicle, waypoints, proximity=0.1)
             log = run_sim(
                 env, traj,
-                lambda _: np.zeros(3, env.vehicle.dtype),
-                max_steps=env.period//env.dt
+                lambda _: np.zeros(3, env.vehicle.dtype)
             )
-            errs.append(np.linalg.norm(log.position[-1]))
+            errs.append(log.reward.sum())
         return np.mean(errs)
     return objective
 
 
 
-def optimize(study: optuna.Study, args: Namespace=DEFAULTS):
+def optimize(args: Namespace=DEFAULTS, seed: int=0):
+    study = get_study(args.study_name, seed=seed)
     study.optimize(make_objective(args), n_trials=args.ntrials//args.nprocs)
     return study
 
 
 
-def get_controller_from_params(**params):
-    pass
+def apply_params(ctrl: Controller, **params) -> Dict[str, Dict[str, np.ndarray]]:
+    p = dict(ctrl_p={}, ctrl_v={}, ctrl_a={}, ctrl_r={}, ctrl_z={}, ctrl_vz={})
+    for name, param in params.items():
+        if '.' in name:
+            pre, post = name.split('.')
+            # skip parameters such as r_pitch_roll.*/r_yaw.*, which need to be combined together
+            if pre.startswith('r_p') or pre.startswith('a_p') or pre.startswith('r_y') or pre.startswith('a_y'):
+                continue
+            p['ctrl_' + pre][post] = param
+        else:
+            p[name] = param
+    # special case for rate controller with differenr pitch/roll, and yaw params
+    # if r_yaw or a_yaw are not present, assume that yaw was not being controlled, and
+    # set yaw params to 0.
+    p['ctrl_r']['k_p'] = np.asarray([params['r_pitch_roll.k_p'], params['r_pitch_roll.k_p'], params.get('r_yaw.k_p', 0)])
+    p['ctrl_r']['k_i'] = np.asarray([params['r_pitch_roll.k_i'], params['r_pitch_roll.k_i'], params.get('r_yaw.k_i', 0)])
+    p['ctrl_r']['k_d'] = np.asarray([params['r_pitch_roll.k_d'], params['r_pitch_roll.k_d'], params.get('r_yaw.k_d', 0)])
+    p['ctrl_r']['max_acceleration'] = np.asarray([params['r_pitch_roll.max_acceleration'],
+                                                  params['r_pitch_roll.max_acceleration'],
+                                                  params.get('r_yaw.max_acceleration', 0)])
+    p['ctrl_a']['k_p'] = np.asarray([params['a_pitch_roll.k_p'], params['a_pitch_roll.k_p'], params.get('a_yaw.k_p', 0)])
+    p['ctrl_a']['k_i'] = np.asarray([params['a_pitch_roll.k_i'], params['a_pitch_roll.k_i'], params.get('a_yaw.k_i', 0)])
+    p['ctrl_a']['k_d'] = np.asarray([params['a_pitch_roll.k_d'], params['a_pitch_roll.k_d'], params.get('a_yaw.k_d', 0)])
+    if ctrl is not None:
+        ctrl.set_params(**p)
+    return p
+
 
 
 if __name__=='__main__':
@@ -148,11 +256,15 @@ if __name__=='__main__':
     parser.add_argument('--ntrials', help='Number of trials.', default=DEFAULTS.ntrials, type=int)
     parser.add_argument('--max_velocity', default=DEFAULTS.max_velocity, type=float)
     parser.add_argument('--max_acceleration', default=DEFAULTS.max_acceleration, type=float)
-    parser.add_argument('--max_angular_acc', default=DEFAULTS.max_angular_acc, type=float)
     parser.add_argument('--max_tilt', default=DEFAULTS.max_tilt, type=float)
+    parser.add_argument('--scurve', action='store_true', default=DEFAULTS.scurve)
+    parser.add_argument('--use_yaw', action='store_true', default=DEFAULTS.use_yaw)
+    parser.add_argument('--wind', help='wind force from heading "force@heading"', default=DEFAULTS.wind)
     parser.add_argument('--bounding_box', default=DEFAULTS.bounding_box, type=float)
     parser.add_argument('--num_sims', default=DEFAULTS.num_sims, type=int)
     parser.add_argument('--append', action='store_true', default=False)
+    parser.add_argument('--pid_params', help='File to save pid params to.', type=str, default='')
+    parser.add_argument('--comment', help='Comments to attach to studdy.', type=str, default='')
     args = parser.parse_args()
 
     if not args.append:
@@ -160,11 +272,18 @@ if __name__=='__main__':
             os.remove(local_path / (args.study_name + '.db'))
         except OSError:
             pass
-
+    
+    # create study if it doesn't exist. The study will be reused with a new seed
+    # by each process
     study = get_study(args.study_name)
 
     for key in vars(args):
         study.set_user_attr(key, getattr(args, key))
 
     with mp.Pool(args.nprocs) as pool:
-        pool.starmap(optimize, [(study, args) for _ in range(args.nprocs)])
+        pool.starmap(optimize, [(args, i) for i in range(args.nprocs)])
+    print(study.best_trial.number)
+    print(study.best_params)
+    if len(args.pid_params) > 0:
+        with open(args.pid_params + ('.pickle' if not args.pidname.endswith('pickle') else ''), 'wb') as f:
+            pickle.dump(apply_params(None, **study.best_params), f)
